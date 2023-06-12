967d4823083b62870492ff1b73b2f4e032d5df4d
CuteKostya <79268579+CuteKostya@users.noreply.github.com>
2023-06-12 15:19:50 +0400

    style code

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home/index')->name('home');
 
 
-Route::get('employees', [EmployeeController::class, 'index'])->name(
-    'employees'
-);
-Route::get('employees/create', [EmployeeController::class, 'create'])->name(
-    'employees.create'
-);
-Route::post('employees', [EmployeeController::class, 'store'])->name(
-    'employees.store'
-);
-Route::get('employees/{employee}', [EmployeeController::class, 'show'])->name(
-    'employees.show'
-);
+Route::get('employees', [EmployeeController::class, 'index'])
+    ->name('employees');
+Route::get('employees/create', [EmployeeController::class, 'create'])
+    ->name('employees.create');
+Route::post('employees', [EmployeeController::class, 'store'])
+    ->name('employees.store');
+Route::get('employees/{employee}', [EmployeeController::class, 'show'])
+    ->name('employees.show');
 Route::get('employees/{employee}/edit', [EmployeeController::class, 'edit'])
     ->name('employees.edit');
-Route::put('employees/{employee}', [EmployeeController::class, 'update'])->name(
-    'employees.update'
-);
+Route::put('employees/{employee}', [EmployeeController::class, 'update'])
+    ->name('employees.update');
 Route::delete('employees/{employee}', [EmployeeController::class, 'delete'])
     ->name('employees.delete');
 
