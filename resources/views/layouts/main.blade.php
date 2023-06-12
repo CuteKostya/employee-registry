7de0fcb601dc5c07bb3f9b6de4e5770e8711ee69
CuteKostya <79268579+CuteKostya@users.noreply.github.com>
2023-06-12 14:56:14 +0400

    corrected register

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,10 +35,10 @@
         <form class="d-flex">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{route('home')}}">Вход </a>
+                    <a class="nav-link" aria-current="page" href="{{route('login')}}">Вход </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{route('employees')}}">Регистрация</a>
+                    <a class="nav-link active" href="{{route('register')}}">Регистрация</a>
                 </li>
             </ul>
         </form>
