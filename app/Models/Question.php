b3a55f06d886f94ed17695bf14c6775bc3bdd523
CuteKostya <79268579+CuteKostya@users.noreply.github.com>
2023-06-27 13:42:55 +0400

    creat model

---
+++ app/Models/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $question
+ * @property string $text
+ * @property string $type
+ * @property string $options
+ */
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable
+        = [
+            'question',
+            'text',
+            'type',
+            'options'
+        ];
+}
