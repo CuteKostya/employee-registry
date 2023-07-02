ecf95a025579aadf4684fff1a9a766e0de3306a2
CuteKostya <79268579+CuteKostya@users.noreply.github.com>
2023-07-02 20:23:42 +0400

    Survey story started

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -55,6 +55,26 @@ class SurveyController extends Controller
     public function store(Request $request)
     {
         //
+
+        dd($request);
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'email' => [
+                'required', 'string', 'max:50', 'email', 'unique:users'
+            ],
+            'password' => [
+                'required', 'string', 'min:7', 'max:50', 'confirmed'
+            ],
+            'agreement' => ['accepted'],
+        ]);
+
+        Survey::query()->create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        return redirect()->route('home');
     }
 
     /**
