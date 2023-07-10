afea106658ac2a5a77e6c3f7ffd2e4a5cc1c73eb
CuteKostya <79268579+CuteKostya@users.noreply.github.com>
2023-07-10 15:52:08 +0400

    10 07

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
 /**
  *
  */
-class Answer extends Authenticatable
+class Answer extends Model
 {
     use HasFactory, Notifiable;
 
